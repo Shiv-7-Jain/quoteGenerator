b3999ae1b07b60e147b5285b03fe4066e5359db3
Shivam Jain <shiv7jain@gmail.com>
2021-11-30 17:03:53 +0530

    Project Ready

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
-const quoteContainer = document.getElementById('quote-container');
+const quoteContainer = document.getElementById('container');
 const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
 
 
 let apiQuotes = []
 
+function loading(){
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+function showQuote(){
+    loader.hidden = true;
+    quoteContainer.hidden = false;
+}
+
 function newQuote(){
+    loading();
     let index = Math.floor(Math.random() * apiQuotes.length);
     let quote = apiQuotes[index];
     if(quote.text.length > 80){
@@ -21,13 +33,17 @@ function newQuote(){
     }else{
         authorText.textContent = 'Unknown'
     }
+    showQuote();
 }
 
+
 async function getQuote(){
+    loading();
     const apiURL = 'https://type.fit/api/quotes';
     try{
         const response = await fetch(apiURL);
         apiQuotes = await response.json();
+        newQuote();
     } catch(error){
         // Catch Error Here
         console.log(error);
